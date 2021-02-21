aaff8a2f927d7510bcde994245a17f930d9155ae
Milos Andrejic <milosandrejic1993@gmail.com>
2021-02-21 15:41:19 +0100

    weather actions implemented

---
+++ redux/dipatchAction.js
@@ -0,0 +1,5 @@
+const dispatchAction = (type, payload, dispatch) => {
+  return dispatch({ type, payload });
+};
+
+export default dispatchAction;
--- utils/apiRequestTypes.js
+++ utils/apiRequestTypes.js
@@ -1,4 +1,4 @@
 export default {
   currentWeather: 'current',
-  dailyForecast: 'daily',
+  dailyForecast: 'forecast/daily',
 };
