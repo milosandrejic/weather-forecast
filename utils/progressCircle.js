43442eebb58966ca79657b0e22e67831cea479b5
Milos Andrejic <milosandrejic1993@gmail.com>
2021-02-24 21:01:20 +0100

    progress animations implemented

---
+++ utils/progressCircle.js
@@ -0,0 +1,11 @@
+export const calculateCircumference = (r) => {
+  return 2 * r * Math.PI;
+};
+
+export const calculateProgress = (circumference, percentage) => {
+  return circumference - (circumference / 100) * percentage;
+};
+
+export const calculateUvIndex = (circumference, value) => {
+  return circumference - (circumference / 10) * value;
+}
