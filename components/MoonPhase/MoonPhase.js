7f3e6cbc59cfd43343f258c405c25e9caea61a99
Milos Andrejic <milosandrejic1993@gmail.com>
2021-03-01 22:55:26 +0100

    added key on moon phase list item

--- components/MoonPhase/MoonPhase.js
+++ components/MoonPhase/MoonPhase.js
@@ -4,10 +4,10 @@ import { connect } from 'react-redux';
 import styles from './MoonPhase.module.scss';
 
 const MoonPhase = (props) => {
-  const { componentIndex, phaseClass, moonPhaseIndex } = props;
-
+  const { componentIndex, phaseClass, moonPhaseIndex, key } = props;
   return (
     <li
+      key={key}
       className={`${styles.moonPhaseItem} ${
         moonPhaseIndex === componentIndex ? styles.moonPhaseItemActive : ``
       } ${phaseClass}`}
@@ -19,6 +19,7 @@ MoonPhase.propTypes = {
   componentIndex: PropTypes.number,
   phaseClass: PropTypes.string,
   moonPhaseIndex: PropTypes.number,
+  key: PropTypes.string,
 };
 
 const mapStateToProps = (state) => ({
